6253ffe659672109d25336cc368ea80443c42196
velarintern <velarinternmail@gmail.com>
2024-01-16 11:49:09 +0500

    filters and styles

--- components/Activity.js
+++ components/Activity.js
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 import { explorerAddress, explorerTx, getFirstAndLast } from "../src/helpers"
 import { transactionInitialState, transactionReducer } from "../store/transaction";
 import { useSelector } from "react-redux";
@@ -6,6 +6,7 @@ import { Loader } from "./CirclLoader";
 
 export const Activity = (props) => {
     const tx = useSelector((x) => x.transaction);
+    const [ search, setSearch ] = useState('');
 
     return (
         <div className="activity-table table-responsive">
@@ -14,64 +15,66 @@ export const Activity = (props) => {
                 <div className="flex-gap-5">
                     <div className="input-with-icon">
                         <img src="/search.svg" alt="" />
-                        <input placeholder="Search deployed tokens" type="text" />
+                        <input value={search} onChange={(e) => setSearch(e.target.value.toLowerCase())} placeholder="Search deployed tokens" type="text" />
                     </div>
                     { tx.transactions.loader && (
                         <Loader width={30} borderWidth={1} customHeight={25} />
                     ) }
                 </div>
             </div>
-            <table>
-                <thead>
-                    <tr>
-                        <th style={{ width: 20 }}>#</th>
-                        <th>Name</th>
-                        <th className="text-right">Status</th>
-                        <th className="text-right">Symbol</th>
-                        <th className="text-right">Decimals</th>
-                        <th className="text-right">Max Supply</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {tx.transactions.data.map((tx, index) => {
-                        return (
-                            <tr key={index}>
-                                <td>{ (index + 1) }</td>
-                                <td className="title">
-                                    <span>{ tx.info.contract_name }</span>
-                                    <a target="_blank" rel="noreferrer" href={explorerTx(tx.tx_id)} className="hash">
-                                        { getFirstAndLast(tx.tx_id) }
-                                        <img src="/ref.svg" alt="" />
-                                    </a>
-                                </td>
-                                <td className="text-right">
-                                    <div className="status flex-end flex-gap-5">
-                                        { tx.status === 'success' ? (
-                                            <>
-                                                <img src="/check_success.svg" alt="" />
-                                                <span className="status-success">Deployed</span>
-                                            </>
-                                        ) : (tx.status === 'pending') ? (
-                                            <>
-                                                <img src="/check_pending.svg" alt="" />
-                                                <span className="status-pending">Deploying</span>
-                                            </>
-                                        ) : (
-                                            <>
-                                                <img src="/check_pending.svg" alt="" />
-                                                <span className="status-error">Failed</span>
-                                            </>
-                                        ) }
-                                    </div>
-                                </td>
-                                <td className="text-white text-right">{ tx.info.symbol }</td>
-                                <td className="text-white text-right">{ tx.info.decimal }</td>
-                                <td className="text-white text-right">{ tx.info.max_supply }</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
+            <div className="activity-table-scrollable">
+                <table>
+                    <thead>
+                        <tr>
+                            <th style={{ width: 20 }}>#</th>
+                            <th>Name</th>
+                            <th className="text-right">Status</th>
+                            <th className="text-right">Symbol</th>
+                            <th className="text-right">Decimals</th>
+                            <th className="text-right">Max Supply</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tx.transactions.data.filter((tx) => tx?.info?.contract_name?.toLowerCase()?.indexOf(search) > -1 ).map((tx, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{ (index + 1) }</td>
+                                    <td className="title">
+                                        <span>{ tx.info.contract_name }</span>
+                                        <a target="_blank" rel="noreferrer" href={explorerTx(tx.tx_id)} className="hash">
+                                            { getFirstAndLast(tx.tx_id) }
+                                            <img src="/ref.svg" alt="" />
+                                        </a>
+                                    </td>
+                                    <td className="text-right">
+                                        <div className="status flex-end flex-gap-5">
+                                            { tx.status === 'success' ? (
+                                                <>
+                                                    <img src="/check_success.svg" alt="" />
+                                                    <span className="status-success">Deployed</span>
+                                                </>
+                                            ) : (tx.status === 'pending') ? (
+                                                <>
+                                                    <img src="/check_pending.svg" alt="" />
+                                                    <span className="status-pending">Deploying</span>
+                                                </>
+                                            ) : (
+                                                <>
+                                                    <img src="/check_pending.svg" alt="" />
+                                                    <span className="status-error">Failed</span>
+                                                </>
+                                            ) }
+                                        </div>
+                                    </td>
+                                    <td className="text-white text-right">{ tx.info.symbol }</td>
+                                    <td className="text-white text-right">{ tx.info.decimal }</td>
+                                    <td className="text-white text-right">{ tx.info.max_supply }</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
